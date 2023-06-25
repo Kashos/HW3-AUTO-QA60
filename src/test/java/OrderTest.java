4cc6cced8697dbab1eca37b9c0309525511fe84b
Egor Kachesov <kashos152@gmail.com>
2023-06-25 10:04:50 +0700

    Add test

---
+++ src/test/java/OrderTest.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class OrderTest {
+
+    @Test
+    void shouldTestOrder() {
+        open("http://localhost:7777/");
+        SelenideElement form = $(By.className("form"));
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+79000000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(By.className("button")).click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
